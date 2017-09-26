e30397ce5df32c23b3bd3cc8ac751922e172b64e
CheriaW <cheria.white@yahoo.com>
2017-09-26 14:19:19 -0400

    Practiced with data tyoes

--- week1_day2/Program.cs
+++ week1_day2/Program.cs
@@ -10,6 +10,26 @@ namespace week1_day2
     {
         static void Main(string[] args)
         {
-        }
+            // data tyoes we've covered thus far; int and string
+            // an int is a whole number (no decimals)
+            //a string is a collection of characters
+
+            char firstInitial = 'D';
+            // Characters or chars are single letters, numbers, or symbols
+
+
+            bool isTired = true;
+            //Boolean or bool is true or false
+
+            //Other number data tyoes
+            float number = 2.16440330497294849383961032f;
+            decimal partialNum = 2.16440330497294849383961032m;
+            double someNum = 2.16440330497294849383961032d;
+
+            Console.WriteLine(number);
+            Console.WriteLine(partialNum);
+            Console.WriteLine(someNum);
+            Console.WriteLine(firstInitial);
+            Console.WriteLine(isTired);        }
     }
 }
